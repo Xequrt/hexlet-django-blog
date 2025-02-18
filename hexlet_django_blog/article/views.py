d2525d455029fc3967fd11b430c326c9a17f8e91
Дмитрий Каблучко <xequrt@yandex.ru>
2025-02-18 20:34:35 +1000

    CRUD list

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views import View
+
+from hexlet_django_blog.article.models import Article
 
 class IndexView(View):
 
     def get(self, request, *args, **kwargs):
-        tags = kwargs.get('tags') if kwargs.get('tags') else 'python'
-        article = kwargs.get('article_id') if kwargs.get('article_id') else 42
-        return HttpResponse(f'Статья номер {article}. Тег {tags}')
-
-# def index(request):
-#     return render(request, 'articles/index.html', context={
-#         'app_name': 'Hexlet Django Blog'
-#     })
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={
+            'articles': articles,
+        })
