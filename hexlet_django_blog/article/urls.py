05adb79883eba697e7e472767a4b65a25ef3e5b9
Дмитрий Каблучко <xequrt@yandex.ru>
2025-02-18 21:40:55 +1000

    Article detail id

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from hexlet_django_blog.article import views
+from hexlet_django_blog.article.views import IndexView, ArticleView
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='article'),
-    path('<str:tags>/<int:article_id>/', views.IndexView.as_view(), name='article_id'),
+    path('', IndexView.as_view(), name='article_index'),
+    path('<int:id>/', ArticleView.as_view(), name='article_id'),
 ]
