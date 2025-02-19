16773cb2e5ad33ea420a836597d318913101af48
Дмитрий Каблучко <xequrt@yandex.ru>
2025-02-19 22:56:27 +1000

    Add update form

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -5,6 +5,27 @@ from .forms import ArticleForm
 
 from hexlet_django_blog.article.models import Article
 
+class ArticleFormEditView(View):
+
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        messages.success(request, 'Данные получены!')
+        return render(request, 'articles/update.html', {'form': form, 'article_id':article_id})
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Данные обновлены!')
+            return redirect('article_index')
+        messages.error(request, 'Произошла ошибка при валидации данных!')
+        return render(request, 'articles/update.html', {'form': form, 'article_id': article_id})
+
+
 class ArticleFormCreateView(View):
 
     def post(self, request, *args, **kwargs):
