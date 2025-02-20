b55934fd066138e41477931ca9febb24c9ebd7d0
Дмитрий Каблучко <xequrt@yandex.ru>
2025-02-20 23:18:28 +1000

    Fix articles_update url

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -5,7 +5,7 @@ from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleForm
 urlpatterns = [
     path('', IndexView.as_view(), name='article_index'),
     path('<int:id>/delete/', ArticleFormDeleteView.as_view(), name='articles_delete'),
-    path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
+    path('<int:id>/update/', ArticleFormEditView.as_view(), name='articles_update'),
     path('<int:id>/', ArticleView.as_view(), name='article_id'),
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
 ]
